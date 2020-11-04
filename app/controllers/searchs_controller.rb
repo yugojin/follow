eebe001a07406f10b20afe71e495e8c55ee2539b
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-11-04 07:18:45 +0000

    [Finish]応用課題５

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,12 @@
+class SearchsController < ApplicationController
+
+  def search
+    @user_or_book = params[:search]
+    @how_search = params[:choice]
+    if @user_or_book == "1"
+       @users = User.search(params[:search], @user_or_book, @how_search)
+    else
+       @books = Book.search(params[:search], @user_or_book, @how_search)
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+
+    def User.search(search, user_or_post, how_search)
+        if user_or_post == "1"
+            if how_search == "1"
+                    User.where(['name LIKE ?', "%#{search}%"])
+            elsif how_search == "2"
+                    User.where(['name LIKE ?', "%#{search}"])
+            elsif how_search == "3"
+                    User.where(['name LIKE ?', "#{search}%"])
+            elsif how_search == "4"
+                    User.where(['name LIKE ?', "#{search}"])
+            else
+                    User.all
+            end
+        end
+    end
+end
